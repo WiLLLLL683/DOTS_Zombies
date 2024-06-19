542370ecccbf371b16af450f21e6e191bb6fe754
WiLLLLL683 <32411970+WiLLLLL683@users.noreply.github.com>
2024-06-19 23:21:39 +0800

    Grounded check

---
+++ Assets/_GAME/Scripts/Grounded/GroundedAuthoring.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using Unity.Entities;
+using Unity.Physics;
+using Unity.Physics.Authoring;
+using UnityEngine;
+
+public class GroundedAuthoring : MonoBehaviour
+{
+    public Vector3 offset;
+    public float threshold;
+    public float maxRayLength;
+    public PhysicsCategoryTags groundLayer;
+    public PhysicsCategoryTags rayCastLayer;
+
+    class Baker : Baker<GroundedAuthoring>
+    {
+        public override void Bake(GroundedAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+
+            AddComponent(entity, new Grounded
+            {
+                offset = authoring.offset,
+                threshold = authoring.threshold,
+                maxRayLength = authoring.maxRayLength,
+                groundLayer = authoring.groundLayer.Value,
+                rayCastLayer = authoring.rayCastLayer.Value
+            });
+        }
+    }
+}
--- Assets/_GAME/Scripts/Grounded/GroundedSystem.cs
+++ Assets/_GAME/Scripts/Grounded/GroundedSystem.cs
@@ -1,8 +1,12 @@
 ﻿using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Physics;
+using Unity.Physics.Systems;
 using Unity.Transforms;
 
+[UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+[UpdateAfter(typeof(PhysicsSystemGroup))]
 [BurstCompile]
 public partial struct GroundedSystem : ISystem
 {
@@ -15,10 +19,41 @@ public partial struct GroundedSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        //foreach (var (transform, grounded) in SystemAPI.Query<LocalTransform, EnabledRefRW<Grounded>>())
-        //{
+        PhysicsWorldSingleton physicsWorldSingleton = SystemAPI.GetSingleton<PhysicsWorldSingleton>();
+        var world = physicsWorldSingleton.CollisionWorld;
 
-        //}
+        foreach (var (transform, enabled, groundedRW) in
+            SystemAPI.Query<LocalTransform, EnabledRefRW<Grounded>, RefRW<Grounded>>()
+            .WithOptions(EntityQueryOptions.IgnoreComponentEnabledState))
+        {
+            ref var grounded = ref groundedRW.ValueRW;
+
+            //параметры рейкаста
+            var raycastInput = new RaycastInput
+            {
+                Start = transform.Position + grounded.offset,
+                End = transform.Position + grounded.offset + (-transform.Up() * grounded.maxRayLength),
+                Filter = new CollisionFilter()
+                {
+                    BelongsTo = grounded.rayCastLayer,
+                    CollidesWith = grounded.groundLayer,
+                    GroupIndex = 0
+                }
+            };
+
+            //рейкаст
+            bool isHit = world.CastRay(raycastInput, out RaycastHit hit);
+            var hitDistance = grounded.maxRayLength * hit.Fraction;
+
+            //дебаг
+            grounded.start = raycastInput.Start;
+            grounded.end = raycastInput.End;
+            grounded.isHit = isHit;
+            grounded.hitDistance = hitDistance;
+
+            //проверка на Grounded
+            enabled.ValueRW = isHit && (hitDistance <= grounded.threshold);
+        }
     }
 }
 
